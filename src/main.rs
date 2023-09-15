d702bdb7d710b38d3119d1214afa115912f7f95d
jl-iii <jesselopez1162@gmail.com>
2023-09-15 09:39:18 -0500

    update readme

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-fn main() -> io:Result<()> {
-    println!("Hello, world!");
+use std::io;
+use crate::io::stdout;
+use std::io::Write;
+
+fn main() -> io::Result<()> {
+    print!("Enter a number: ");
+    let _ = stdout().flush()?;
+
+    let stdin = io::read_to_string(io::stdin())?;
+    println!("{stdin}");
+
     let mut buffer = String::new();
     let stdin = io::stdin();
     stdin.read_line(&mut buffer)?;
+    print!("You entered \"{}\"", &mut buffer);
     Ok(())
 }

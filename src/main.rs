3bf6412999b3c204d7a1ea2de3cce46d25621744
jl-iii <jesselopez1162@gmail.com>
2023-09-15 10:20:10 -0500

    begin validate arguments function

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,15 @@ fn main() -> io::Result<()> {
     let stdin = io::stdin();
     stdin.read_line(&mut buffer)?;
     print!("You entered {}", &mut buffer);
+    validate_arguments(&buffer);
     Ok(())
+}
+
+fn validate_arguments(s: &str) {
+    print!("Inside validate_arguments {}", s);
+    for word in s.split_whitespace() {
+        println!("> {}", word);
+        //Validate here
+        //then store in vec
+    }
 }

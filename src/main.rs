3db83a4fc7d2ba73c6ed45812bf2e2d4aaf9abb0
jl-iii <jesselopez1162@gmail.com>
2023-09-15 10:04:28 -0500

    record user input

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@ use crate::io::stdout;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
-    print!("Enter a number: ");
+    print!("Enter an expression: ");
     let _ = stdout().flush()?;
-
-    let stdin = io::read_to_string(io::stdin())?;
-    println!("{stdin}");
 
     let mut buffer = String::new();
     let stdin = io::stdin();
     stdin.read_line(&mut buffer)?;
-    print!("You entered \"{}\"", &mut buffer);
+    print!("You entered {}", &mut buffer);
     Ok(())
 }

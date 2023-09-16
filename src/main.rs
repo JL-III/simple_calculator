eb0837d81a1e3568eef7b0e34e78c0e42e861cfb
jl-iii <jesselopez1162@gmail.com>
2023-09-16 13:43:01 -0500

    validate arguments

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,51 @@
-use std::io;
 use crate::io::stdout;
+use std::io;
+use std::io::Stdin;
 use std::io::Write;
+use std::process::exit;
 
 fn main() -> io::Result<()> {
-    print!("Enter an expression: ");
     let _ = stdout().flush()?;
-
-    let mut buffer = String::new();
     let stdin = io::stdin();
-    stdin.read_line(&mut buffer)?;
-    print!("You entered {}", &mut buffer);
-    validate_arguments(&buffer);
+    // print!("{}", get_three_arguments(stdin));
+    let _ = validate_arguments(&get_three_arguments(stdin));
     Ok(())
 }
 
-fn validate_arguments(s: &str) {
-    print!("Inside validate_arguments {}", s);
-    for word in s.split_whitespace() {
-        println!("> {}", word);
-        //Validate here
-        //then store in vec
+fn get_three_arguments(stdin: Stdin) -> String {
+    let mut buffer = String::new();
+    while &buffer.split_whitespace().collect::<Vec<_>>().len() != &3 {
+        println!("\nEnter 3 arguments.\nExample: 8 * 3");
+        print!("> ");
+        let _ = stdout().flush();
+
+        buffer = String::new();
+        stdin.read_line(&mut buffer);
+    }
+    buffer
+}
+
+fn validate_arguments(s: &str) -> Vec<&str> {
+    let mut args: Vec<&str> = s.split_whitespace().collect();
+    if is_parsable_to_i32(args[0]) && is_mathematical_operator(args[1]) && is_parsable_to_i32(args[2]) {
+        print!("Valid inputs!\n");
+    } else {
+        print!("Please enter a mathematical expression\nExample: 8 * 3\n");
+        exit(1)
+    }
+    args
+}
+
+fn is_parsable_to_i32(s: &str) -> bool {
+    s.parse::<i32>().is_ok()
+}
+
+fn is_mathematical_operator(s: &str) -> bool {
+    //symbols.contains(s)
+    let vec_of_operators: Vec<&str> = vec!["*", "/", "+", "-"];
+    if vec_of_operators.contains(&s) {
+        true
+    } else {
+        false
     }
 }
